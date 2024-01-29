20110b05f4b447c39dab4047b39108ccbf8947c0
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-28 21:11:27 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_NestedIfs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
